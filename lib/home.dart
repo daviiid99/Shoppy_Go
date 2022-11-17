349a31b52a720da2d14b3ee24f08024847441b4b
daviiid99 <daviiideveloper@gmail.com>
2022-11-17 01:05:32 +0100

    Add initial support for exporting a note as a pdf file and share it

--- lib/home.dart
+++ lib/home.dart
@@ -19,7 +19,7 @@ class _HomeState extends State<Home>{
 
   String jsonFile = "products.json";
   String jsonString = "";
-  String version = "1.5";
+  String version = "2.0";
 
   Map<dynamic, dynamic> products = {
 
